b4a4ca3f3dc0d84471cdf8b85ac7ae802247d1f3
Matt Zanchelli <matt@mdznr.com>
2012-03-30 20:57:37 -0400

    Keyboard Shortcut
    Shift+Alt+I

--- Send to Instapaper.safariextension/readlater.js
+++ Send to Instapaper.safariextension/readlater.js
@@ -1,8 +1,24 @@
 
-safari.self.addEventListener("message", run, false);
+window.addEventListener("keydown", keyboardShortcut, false);
+
+function keyboardShortcut()
+{
+	if ( event.target.nodeName.toLowerCase() !== 'input' ) {
+		if ( event.altKey && event.keyCode == "73" ) {
+			safari.self.tab.dispatchMessage( "shortcut", true);
+			cmon("QMMUOk2ZEBse");
+		}
+	}
+}
+
+safari.self.addEventListener( "message", run, false );
 
 function run(msg) {
-	var code = msg.message;
+	cmon(msg.message);
+}
+
+function cmon(code) {
+	var code = "QMMUOk2ZEBse";
 	var d = document, z = d.createElement('scr'+'ipt'), b = d.body, l = d.location;
 	try {
 		if (!b) throw(0);
@@ -11,6 +27,6 @@ function run(msg) {
 		b.appendChild(z);
 	}
 	catch(e) {
-		alert('Please wait until the page has loaded.');
+		alert( 'Please wait until the page has loaded.' );
 	}
 }
