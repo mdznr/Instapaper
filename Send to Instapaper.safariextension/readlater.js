56c1210f90fff029a7700bce772a4e38291bb71b
Matt Zanchelli <matt@mdznr.com>
2012-04-10 21:17:57 -0400

    Icons! Null link fix

--- Send to Instapaper.safariextension/readlater.js
+++ Send to Instapaper.safariextension/readlater.js
@@ -13,12 +13,12 @@ function handleContextMenu(event) {
 }
 
 //	Listen for keyboard commands
-window.addEventListener("keydown", keyboardShortcut, false);
+window.addEventListener("keyDown", keyboardShortcut, false);
 
 //	Execute command on keyboard shortcut (Customisable?)
 function keyboardShortcut() {
 	if ( event.target.nodeName.toLowerCase() !== 'input' ) {
-		if ( event.altKey && event.keyCode == "73" ) {	//	Shift + Alt (Option) + I
+		if ( event.keyCode == "73" ) {	//	Shift + Alt (Option) + I
 			safari.self.tab.dispatchMessage("keyboardShortcut", true);
 		}
 	}
@@ -41,18 +41,17 @@ function displayResults(response) {
 		if ( response.message == 200 || response.message == 201 ) {
 			results.className = "success";
 			results.innerHTML = "Sent to Instapaper!";
-		} else if ( response.message == 403 ) {
-			results.className = "failed";
-			results.innerHTML = "Invalid username or password.";
-		} else if ( response.message == 400 ) {
-			results.className = "failed";
-			results.innerHTML = "Bad request or exceeded the rate limit. Check your Preferences.";
-		} else if ( response.message == 500 ) {
-			results.className = "failed";
-			results.innerHTMl = "The service encountered an error. Please try again later.";
 		} else {
 			results.className = "failed";
-			results.innerHTMl = "Unexpected error: " + response.message;
+			if ( response.message == 403 ) {
+				results.innerHTML = "Invalid username or password.";
+			} else if ( response.message == 400 ) {
+				results.innerHTML = "Bad request or exceeded the rate limit. Check your Preferences.";
+			} else if ( response.message == 500 ) {
+				results.innerHTMl = "The service encountered an error. Please try again later.";
+			} else {
+				results.innerHTMl = "Unexpected error: " + response.message;
+			}
 		}
 		setTimeout( "hideResults()", 2500 );
 	}
