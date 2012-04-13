7288719c0bef5a535447ca8acd5082178d282fef
Matt Zanchelli <matt@mdznr.com>
2012-04-12 23:22:21 -0400

    Semi-customisable keyboard shortcuts, fixed issue where right-click on non-link would send wrong URL to Instapaper.

--- Send to Instapaper.safariextension/readlater.js
+++ Send to Instapaper.safariextension/readlater.js
@@ -4,8 +4,12 @@ window.addEventListener("contextmenu", handleContextMenu, false);
 
 //	Disable menu item if not a link?
 function handleContextMenu(event) {
-	if ( event.target.nodeName != "A" ) {
+	// console.log("Contextual Menu");
+	console.log( event.target.nodeName.toLowerCase() );
+	if ( event.target.nodeName.toLowerCase() != "a" ) {
+		//	Get disabling to work and add on/off preference for this feature
 		safari.self.tab.dispatchMessage("disable", true);
+		safari.self.tab.dispatchMessage("getLink", document.URL);
 	} else {
 		safari.self.tab.dispatchMessage("getLink", event.target.href);
 		console.log(event.target.href);	//	For debugging
@@ -13,14 +17,13 @@ function handleContextMenu(event) {
 }
 
 //	Listen for keyboard commands
-window.addEventListener("keyDown", keyboardShortcut, false);
+window.addEventListener("keydown", keyboardShortcut, false);
 
-//	Execute command on keyboard shortcut (Customisable?)
+//	Execute command on keyboard shortcut
 function keyboardShortcut() {
-	if ( event.target.nodeName.toLowerCase() !== 'input' ) {
-		if ( event.keyCode == "73" ) {	//	Shift + Alt (Option) + I
-			safari.self.tab.dispatchMessage("keyboardShortcut", true);
-		}
+	console.log(event.keyCode);
+	if ( event.target.nodeName.toLowerCase() !== "input" && event.altKey ) {
+		safari.self.tab.dispatchMessage("keyboardShortcut", event.keyCode);
 	}
 }
 
